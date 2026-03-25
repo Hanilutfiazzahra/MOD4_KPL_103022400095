33aba72181a30ede7334de0ae6cc5f8773ec5f8b
Hanilutfiazzahra <hanilutfiazzahraa@gmail.com>
2026-03-25 07:14:02 +0700

    Second commit

---
+++ Modul4_103022400095/KodePaket.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Modul4_103022400095
+{
+    internal class KodePaket
+    {
+        private Dictionary<string, string> tabelKode = new Dictionary<string, string>()
+        {
+            {"Basic", "P201"},
+            {"Standard", "P202"}, 
+            {"Premium", "P203" },
+            {"Unlimited", "P204"},
+            {"Gaming", "P205"},
+            {"Streaming", "P06"},
+            {"Family", "P07" },
+            {"Business", "P08"},
+            {"Student", "P09"},
+            {"Traveler", "P10"}
+        };
+
+        public string GetKode(string namaPaket)
+        {
+            if (tabelKode.ContainsKey(namaPaket))
+            {
+                return tabelKode[namaPaket];
+            }
+            else
+            {
+                return "Kode paket tidak ditemukan.";
+            }
+        }
+    }
+}
--- Modul4_103022400095/Program.cs
+++ Modul4_103022400095/Program.cs
@@ -1,1 +1,23 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Modul4_103022400095
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            KodePaket kp = new KodePaket();
+
+            Console.WriteLine("Kode Paket Basic = " + kp.GetKode("Basic"));
+            Console.WriteLine("Kode Paket Standard = " + kp.GetKode("Standard"));
+            Console.WriteLine("Kode Paket Premium = " + kp.GetKode("Premium"));
+            Console.WriteLine("Kode Paket Unlimited = " + kp.GetKode("Unlimited"));
+            Console.WriteLine("Kode Paket Gaming = " + kp.GetKode("Gaming"));
+            Console.WriteLine("Kode Paket Streaming = " + kp.GetKode("Streaming"));
+            Console.WriteLine("Kode Paket Family = " + kp.GetKode("Family"));
+            Console.WriteLine("Kode Paket Business = " + kp.GetKode("Business"));
+            Console.WriteLine("Kode Paket Student = " + kp.GetKode("Student"));
+            Console.WriteLine("Kode Paket Traveler = " + kp.GetKode("Traveler"));
+        }
+    }
+}
